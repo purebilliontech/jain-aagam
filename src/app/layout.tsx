c63f17130f0c1c97b2086cb103d44a3bfa5a14d3
programmerpranit <dev.pranitpatil@gmail.com>
2025-04-14 05:44:24 +0000

    fix root layout issue

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import '../globals.css';
+
+
+export const metadata: Metadata = {
+  title: "Jain Agam",
+  description: "Explore the timeless wisdom and teachings of Jain Agam, a collection of sacred texts that form the foundation of Jain philosophy and spirituality.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet" />
+      </head>
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
