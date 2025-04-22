4858fe0569dc0e9011a46283ffbe0ad904351ea9
programmerpranit <dev.pranitpatil@gmail.com>
2025-04-22 18:52:50 +0000

    removed bcrypt lib

--- src/app/auth/login/action.tsx
+++ src/app/auth/login/action.tsx
@@ -14,7 +14,8 @@ export const login = async (data: Login) => {
     try {
         const pass = await encryptPassword(data.password);
 
-        const comparision = await comparePassword(data.password, pass);
+        // const comparision = await comparePassword(data.password, pass);
+        const comparision = await comparePassword(data.password, "$2b$10$oIgbccEBau1JMfgD1OJnWOb5Mj0bH5l6factfv4b2db01w.YcdcxK");
         console.log("Comparision", comparision)
 
         console.log(pass);
