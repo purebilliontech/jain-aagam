e94175c5c56afd83e0b8b6244962f484e86d2dba
prathameshkarambelkar <pkarambelkar.2002@gmail.com>
2025-04-22 07:21:31 +0530

    create videos page with yt thumbnail, improve responsiveness for blogs page

--- src/app/(public)/(home)/(sections)/TopSection.tsx
+++ src/app/(public)/(home)/(sections)/TopSection.tsx
@@ -4,13 +4,13 @@ const TopSection = () => {
   return (
     <section className="min-h-screen flex flex-col md:flex-row items-center justify-center md:justify-around bg-[#e9e2d2] mx-auto p-4">
       <div className="text-center md:text-left ">
-        <h2 className="text-[#C6AF8B] font-sans text-5xl md:text-8xl font-semibold">
+        <h2 className="text-primary-ui font-sans text-5xl md:text-8xl font-semibold">
           JAIN
         </h2>
-        <h2 className="text-[#C6AF8B] font-sans text-5xl md:text-8xl font-semibold">
+        <h2 className="text-primary-ui font-sans text-5xl md:text-8xl font-semibold">
           AAGAM
         </h2>
-        <h3 className="text-[#C6AF8B] font-mono text-2xl md:text-4xl font-medium leading-normal mt-6">
+        <h3 className="text-primary-ui font-mono text-2xl md:text-4xl font-medium leading-normal mt-6">
           The holy scriptures of Jainism
         </h3>
       </div>
