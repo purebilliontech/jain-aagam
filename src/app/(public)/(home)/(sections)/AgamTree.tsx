367bd5537833bcca933c944dc45d2bd99df55c48
programmerpranit <dev.pranitpatil@gmail.com>
2025-05-25 05:05:28 +0000

    fixed animations

--- src/app/(public)/(home)/(sections)/AgamTree.tsx
+++ src/app/(public)/(home)/(sections)/AgamTree.tsx
@@ -2,36 +2,65 @@
 
 import Typography from '@/components/common/typography'
 import Image from 'next/image'
-import React, { Dispatch, SetStateAction, useState } from 'react'
+import React, { useRef } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
-const TreeMarker = ({ className, title, subtitle, active, setActive, number }: { className: string, title: string, subtitle: string, active: number, setActive: Dispatch<SetStateAction<number>>, number: number }) => {
+const TreeMarker = ({ className, title, subtitle, number }: { className: string, title: string, subtitle: string, number: number }) => {
+    const ref = useRef(null);
+    const [isVisible, setIsVisible] = React.useState(false);
+
+    React.useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsVisible(entry.isIntersecting);
+            },
+            {
+                threshold: 1
+            }
+        );
+
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, []);
 
     return (
-        <div className={`absolute  ${className}`} >
-            <div onClick={() => setActive((prev: number) => prev === number ? 0 : number)} className="bg-[#E3DFD5] cursor-pointer sm:w-16 sm:h-16 w-12 h-12 p-2 text-center flex justify-center items-center rounded-full">
+        <div ref={ref} className={`absolute ${className}`}>
+            <div className="bg-[#E3DFD5] sm:w-16 sm:h-16 w-12 h-12 p-2 text-center flex justify-center items-center rounded-full">
                 <Typography variant='h4' className='!sm:text-sm !text-xs leading-tight w-full italic text-typography'>{title}</Typography>
             </div>
-            <div className={`absolute -top-5 border-l-8 border-primary-ui bg-white/50 left-16 md:left-44 sm:left-36 sm:pl-3 p-1 md:min-w-52 w-28 sm:min-w-40 ${active === number ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300 `}>
-                <Typography variant='h4' className='w-full italic text-typography max-sm:text-sm'>{title}</Typography>
-                <Typography variant='p' className='!sm:text-base w-full font-mono  italic text-typography/70 !text-xs leading-none'>{subtitle}</Typography>
-            </div>
-        </div >
+            <AnimatePresence>
+                {isVisible && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 1 }}
+                        className="absolute -top-5 border-l-8 border-primary-ui bg-white/50 left-16 md:left-44 sm:left-36 sm:pl-3 p-1 md:min-w-52 w-28 sm:min-w-40"
+                    >
+                        <Typography variant='h4' className='w-full italic text-typography max-sm:text-sm'>{title}</Typography>
+                        <Typography variant='p' className='!sm:text-base w-full font-mono italic text-typography/70 !text-xs leading-none'>{subtitle}</Typography>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div>
     )
 }
 
-
 export default function AgamTree() {
-
-    const [active, setActive] = useState(0);
-
     return (
         <>
             <div className="max-w-7xl mx-auto relative my-20 md:h-screen">
                 <Image src={'/static/home/tree.png'} alt='Jain Agam Map' className='w-full h-full object-contain max-sm:p-5' width={2000} height={1000} />
-                <TreeMarker className='top-[30%] left-[50%]' title="Upang Aagam" subtitle='Just like the branches and leaves form a part of the tree' active={active} setActive={setActive} number={3} />
-                <TreeMarker className='sm:top-[50%] top-[47%] left-[48%]' title="Ang Aagam" subtitle='The core texts like the trunk of a tree' active={active} setActive={setActive} number={1} />
-                <TreeMarker className='sm:top-[70%] top-[67%] left-[50%]' title="Mul Aagam" subtitle='The roots or fundamental basics' active={active} setActive={setActive} number={2} />
-
+                <TreeMarker className='top-[30%] left-[50%]' title="Upang Aagam" subtitle='Just like the branches and leaves form a part of the tree' number={3} />
+                <TreeMarker className='sm:top-[50%] top-[47%] left-[48%]' title="Ang Aagam" subtitle='The core texts like the trunk of a tree' number={1} />
+                <TreeMarker className='sm:top-[70%] top-[67%] left-[50%]' title="Mul Aagam" subtitle='The roots or fundamental basics' number={2} />
             </div>
         </>
     )
--- src/app/(public)/(home)/(sections)/JainAgamMap.tsx
+++ src/app/(public)/(home)/(sections)/JainAgamMap.tsx
@@ -2,29 +2,57 @@
 
 import Typography from '@/components/common/typography'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
+const MapMarker = ({ className, title, subtitle, up = false }: { className: string, title: string, subtitle: string, up: boolean }) => {
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = React.useState(false);
 
-const MapMarker = ({ className, title, subtitle }: { className: string, title: string, subtitle: string }) => {
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 1
+      }
+    );
 
-  const [isClicked, setIsClicked] = useState(false);
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
 
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
 
   return (
-    <div className={`absolute ${className}`} >
-      <div onClick={() => setIsClicked(!isClicked)} className="bg-accent-ui w-5 h-5 flex justify-center items-center rounded-full">
+    <div ref={ref} className={`absolute ${className}`}>
+      <div className="bg-accent-ui w-5 h-5 flex justify-center items-center rounded-full">
         <div className="bg-accent-ui w-5 h-5 md:w-3 md:h-3 rounded-full animate-ping">
         </div>
       </div>
-      <div className={`absolute top-5 -left-5 min-w-28 sm:left-3 p-3 md:min-w-40  ${isClicked ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300 bg-[#D9D2C0] rounded-lg `}>
-        {/* <div className="w-6 h-6 bg-white rounded-full"></div> */}
-        <Typography variant='h4' className='!text-base w-full italic text-typography'>{title}</Typography>
-        <Typography variant='h4' className='!text-base italic text-typography/70'>{subtitle}</Typography>
-      </div>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className={`absolute ${up ? '-left-5 bottom-5' : '-right-5 top-5'} min-w-28 sm:left-3 p-3 md:min-w-40 bg-[#D9D2C0] rounded-lg`}
+          >
+            <Typography variant='h4' className='!text-base w-full italic text-typography'>{title}</Typography>
+            <Typography variant='h4' className='!text-base italic text-typography/70'>{subtitle}</Typography>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
-
 
 export default function JainAgamMap() {
   return (
@@ -32,12 +60,9 @@ export default function JainAgamMap() {
       <div className="max-w-7xl mx-auto relative my-20">
         <Image src={'/static/home/map.png'} alt='Jain Agam Map' width={2000} height={1000} />
 
-        <MapMarker className='top-[25%] left-[5%]' title='Jain Center' subtitle='North California' />
-        <MapMarker className='top-[29%] left-[8%]' title='Jain Center' subtitle='South California' />
-
-
-        <MapMarker className='top-[20%] left-[50%]' title='The British Library' subtitle='London, UK' />
-
+        <MapMarker className='top-[25%] left-[5%]' title='Jain Center' subtitle='North California' up={true} />
+        <MapMarker className='top-[29%] left-[8%]' title='Jain Center' subtitle='South California' up={false} />
+        <MapMarker className='top-[20%] left-[50%]' title='The British Library' subtitle='London, UK' up={true} />
       </div>
     </>
   )
