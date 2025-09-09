d952933d2267781dff8a4d221fd9e5b7e384e65b
programmerpranit <dev.pranitpatil@gmail.com>
2025-09-09 16:48:24 +0530

    add button

--- src/app/(public)/art-gallery/(sections)/Timings.tsx
+++ src/app/(public)/art-gallery/(sections)/Timings.tsx
@@ -5,11 +5,13 @@ import React from 'react'
 export default function TimingsSection() {
     return (
         <>
-            <div className='max-w-7xl mx-auto p-5 py-10 pt-20'>
+            <div id='timings' className='max-w-7xl mx-auto p-5 py-10 pt-20'>
                 <SectionTitle title='An Immersive Experience' subtitle='EXPLORE' className='pb-10' />
 
                 <Typography variant='p' className='text-justify mx-auto w-fit text-typography '>Visit Parasdham Girnar for an exclusive, experiential display of the Aaradhya Art Gallery </Typography>
 
+                {/* <Typography variant='h5' className='text-foreground-ui text-center mt-5'>Venue</Typography> */}
+                <Typography variant='p' className='text-justify mx-auto mt-3 w-fit text-foreground-ui'>Parasdham Girnar, Rupayatan Road, Bhavnath Taleti, Girnar, Junagadh – 362004</Typography>
                 <div className="flex md:flex-row flex-col gap-10 py-10">
                     <div className="md:w-1/2">
                         <Typography variant='h5' className='text-foreground-ui text-center mb-2'>Weekday Visiting Hours</Typography>
--- src/app/(public)/art-gallery/(sections)/TopSection.tsx
+++ src/app/(public)/art-gallery/(sections)/TopSection.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 
@@ -5,19 +6,25 @@ const TopSection = () => {
   return (
     <section className="relative w-screen h-[35vh] sm:h-[40vh] md:h-[70vh]">
       <Image
-        src={"/static/banners/ArtGalleryDesktop.png"}
+        src={"/static/banners/ArtVenue.png"}
         alt={'Art Gallery Banner'}
         width={2000}
         height={2000}
         className="w-screen h-full  absolute -z-50 top-0 left-0 hidden md:block"
       />
-       <Image
-        src={"/static/banners/ArtGalleryMobile.png"}
+      <Image
+        src={"/static/banners/ArtVenue.png"}
         alt={'Art Gallery Banner'}
         width={2000}
         height={2000}
         className="w-screen h-full block md:hidden  absolute -z-50 top-0 left-0"
       />
+      <a href="#timings" className="absolute bottom-5 right-[20%] z-50">
+        <Button variant="secondary" className="bg-accent-ui text-white shadow-xs ">
+          Timings
+        </Button>
+      </a>
+
     </section>
   );
 };
